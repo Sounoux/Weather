ddf45104f7666e1d382e537e4741cf386d9ce57c
Sounoux <73953629+Sounoux@users.noreply.github.com>
2022-01-27 18:58:38 +0100

    Add files via upload

--- lib/current.dart
+++ lib/current.dart
@@ -3,6 +3,7 @@ import 'models/forcast.dart';
 import 'models/location.dart';
 import 'models/weather.dart';
 import 'api.dart';
+import 'search.dart';
 import 'package:intl/intl.dart';
 
 class CurrentWeatherPage extends StatefulWidget {
@@ -147,139 +148,6 @@ Widget createAppBar(
               )),
         ],
       ));
-}
-
-class CitySearch extends SearchDelegate<Location> {
-  List<Location>? locations;
-  @override
-  List<Widget> buildActions(BuildContext context) => [
-        IconButton(
-          icon: const Icon(Icons.clear),
-          onPressed: () {
-            if (query.isEmpty) {
-              close(
-                  context,
-                  Location(
-                      city: "paris",
-                      country: "france",
-                      lat: "48.853",
-                      lon: "2.349"));
-            } else {
-              query = '';
-              showSuggestions(context);
-            }
-          },
-        )
-      ];
-
-  @override
-  Widget buildLeading(BuildContext context) => IconButton(
-        icon: const Icon(Icons.arrow_back),
-        onPressed: () => close(
-            context,
-            Location(
-                city: "paris", country: "france", lat: "48.853", lon: "2.349")),
-      );
-
-  @override
-  Widget buildResults(BuildContext context) => FutureBuilder<Weather?>(
-        future: WeatherApi.getWeather(city: query),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const Center(child: CircularProgressIndicator());
-            default:
-              print(snapshot.hasData);
-              if (snapshot.hasError) {
-                return Container(
-                  color: Colors.black,
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Quelque chose s est mal passé!',
-                    style: TextStyle(fontSize: 28, color: Colors.white),
-                  ),
-                );
-              } else {
-                return CurrentWeatherPage(
-                    locations = [
-                      Location(
-                          city: snapshot.data!.city,
-                          country: snapshot.data!.country,
-                          lat: snapshot.data!.lat.toString(),
-                          lon: snapshot.data!.long.toString())
-                    ],
-                    context);
-              }
-          }
-        },
-      );
-
-  @override
-  Widget buildSuggestions(BuildContext context) => Container(
-        color: Colors.black,
-        child: FutureBuilder<List<String>>(
-          future: WeatherApi.searchCities(query: query),
-          builder: (context, snapshot) {
-            if (query.isEmpty) return buildNoSuggestions();
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return const Center(child: CircularProgressIndicator());
-              default:
-                if (snapshot.hasError || snapshot.data!.isEmpty) {
-                  return buildNoSuggestions();
-                } else {
-                  return buildSuggestionsSuccess(snapshot.data!);
-                }
-            }
-          },
-        ),
-      );
-
-  Widget buildSuggestionsSuccess(List<String> suggestions) => ListView.builder(
-        itemCount: suggestions.length,
-        itemBuilder: (context, index) {
-          final suggestion = suggestions[index];
-          final queryText = suggestion.substring(0, query.length);
-          final remainingText = suggestion.substring(query.length);
-
-          return ListTile(
-            onTap: () {
-              query = suggestion;
-              showResults(context);
-            },
-            leading: const Icon(Icons.location_city),
-            title: RichText(
-              text: TextSpan(
-                text: queryText,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-                children: [
-                  TextSpan(
-                    text: remainingText,
-                    style: const TextStyle(
-                      color: Colors.white54,
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
-      );
-
-  Widget buildNoSuggestions() => const Center(
-        child: Text(
-          'Pas de suggestion!',
-          style: TextStyle(fontSize: 28, color: Colors.white),
-        ),
-      );
-
-  @override
-  noSuchMethod(Invocation invocation) => super.noSuchMethod(invocation);
 }
 
 Widget weatherDetailsBox(Weather _weather) {
