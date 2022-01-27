91af35fbd918b169991b443c581419fb2f38beff
Sounoux <73953629+Sounoux@users.noreply.github.com>
2022-01-27 17:47:01 +0100

    Delete extensions.dart

--- lib/extensions.dart
+++
@@ -1,6 +0,0 @@
-extension StringExtension on String {
-  String get inCaps => '${this[0].toUpperCase()}${substring(1)}';
-  String get allInCaps => toUpperCase();
-  String get capitalizeFirstOfEach =>
-      split(" ").map((str) => str.inCaps).join(" ");
-}
